b1d6fe468970f116ae912f68be4a5685bd47ed94
Liuxs <lxs1992nmgdx@163.com>
2020-03-04 21:09:56 +0800

    旋转数组1通过修改期望值旋转数组

--- RotateArray.java
+++ RotateArray.java
@@ -10,22 +10,19 @@ package com.geek.work;
 public class RotateArray {
     public void rotate(int[] nums, int k) {
         if (nums == null || nums.length < 2) return;
-        k %= nums.length;
-        for (int i = nums.length - 1; i > k - 1; i--) {
-            exchange(nums, i, i - k);
-        }
-        if (nums.length % k == 0) return;
-        rotateRight(nums, 0, nums.length - (nums.length % k) - 1, Math.abs(nums.length % k - k));
-
+        rotateRight(nums, 0, nums.length - 1, k);
     }
 
     private void rotateRight(int[] nums, int start, int end, int k) {
+        k %= end + 1;
         if (start >= end || k <= 0) return;
         for (int i = end;  i > k - 1 && i > start; i--) {
             exchange(nums, i , i - k );
         }
         if ((end + 1)% k == 0) return;
-        rotateRight(nums,  start , end - end % k - 1, Math.abs((end + 1) % k - k));
+        int stemp = Math.abs((end + 1) % k - k);
+        end = end - ((end + 1) % k) - (((end + 1) / k) - 1) * k;
+        rotateRight(nums,  start , end, stemp);
     }
 
     /**
@@ -43,7 +40,7 @@ public class RotateArray {
     public static void main(String[] args) {
         RotateArray rotateArray = new RotateArray();
         int[] nums = {1, 2, 3, 4, 5, 6, 7};
-        rotateArray.rotate(nums, 4);
+        rotateArray.rotate(nums, 7);
         for (int m:
              nums) {
             System.out.println(m);
